4ee6ad9e334b12802f40dd22d946dda398f24b1f
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-06-28 13:15:47 -0300

    atualização

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: 'public',
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Porta padrão ou escolha outra se necessário
+    open: true, // Abre automaticamente no navegador
+    host: true, // Permite acessar pelo IP da rede local
+  },
+});
