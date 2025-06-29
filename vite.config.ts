305590d1169c4b090eb0789ca51122197ba82be7
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-06-28 23:05:29 -0300

    fix: corrigindo erro do caminho  das pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  publicDir: 'public',
   plugins: [react()],
-  server: {
-    port: 5173, // Porta padrão ou escolha outra se necessário
-    open: true, 
-    host: true,
-  },
+ 
 });
