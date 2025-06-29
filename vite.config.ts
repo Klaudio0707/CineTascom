ef7d28536ef75ab69bb17fe3b1753114560efa6d
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-06-28 21:24:00 -0300

    scripts build do arquivo package.json- config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; 
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
     },
+  },
+  publicDir: 'public',
+  plugins: [react()],
+  server: {
+    port: 5173, 
+    open: true,
+    host: true, 
   },
 });
